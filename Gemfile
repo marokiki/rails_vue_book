7a94c6aac9c0f0a6be24669f21beb66c0a8259ed
segre <segrehouck5458@gmail.com>
2024-05-21 20:53:34 +0900

    add user

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'rails', '~> 7.0.4'
+
+gem "devise"
--- apps/config/routes.rb
+++ apps/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   resources :rooms
 
   root "rooms#index"
+
+  devise_for :admins, controllers: {
+                        registrations: "admins/registrations",
+                        sessions: "admins/sessions",
+                      }
+
+  devise_for :users, controllers: {
+                       registrations: "users/registrations",
+                       sessions: "users/sessions",
+                     }
 end
