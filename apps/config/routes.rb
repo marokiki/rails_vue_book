51cc9b8a4f635a55d76f04f2a5b4c2a3962df1f1
segre <segrehouck5458@gmail.com>
2024-05-21 19:33:07 +0900

    room

--- apps/config/routes.rb
+++ apps/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :calenders
+  resources :rooms
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "rooms#index"
 end
